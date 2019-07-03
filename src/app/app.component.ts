97f41faa9248dba0d76d3f177cbafb1b1a5e80b2
Ouassim <ouassim.benmosbah@gmail.com>
2019-07-03 17:05:51 +0200

    Add partially editable grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { ColDef, GridOptions } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ag-grid-test';
+  columnDefs: ColDef[] = [
+    { headerName: 'Nom', field: 'name' },
+    { headerName: 'Liste', field: 'list' },
+    {
+      headerName: 'Unité',
+      field: 'unit',
+      editable: params => {
+        return params.data.list === 'Off';
+      }
+    },
+    { headerName: 'Liste de choix', field: 'choiceList' }
+  ];
+
+  rowData = [
+    {
+      name: 'NB Logement vendu',
+      list: 'Off',
+      unit: 'U',
+      choiceList: null
+    },
+    {
+      name: 'Zone',
+      list: 'On',
+      unit: null,
+      choiceList: ['senegal', 'brest'].join('; ')
+    },
+    {
+      name: 'Taux d occupation',
+      list: 'Off',
+      unit: '%',
+      choiceList: null
+    },
+    { name: 'birame', list: 'Off', unit: 'O', choiceList: null }
+  ];
+
+  gridOptions: GridOptions = {
+    columnDefs: this.columnDefs,
+    rowData: this.rowData,
+    singleClickEdit: true,
+    defaultColDef: {
+      editable: true
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AgGridModule.withComponents([])],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
